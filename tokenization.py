7834974b98b8c0aea9bf6bde2bdc4fd98ca7b393
sl394@duke.edu <sl394@duke.edu>
2019-11-12 17:24:04 -0500

    debug

--- tokenization.py
+++ tokenization.py
@@ -134,7 +134,7 @@ def load_vocab(vocab_file):
 	return vocab
 
 def load_chars_vocab(chars_vocab_file):
-	token2charsid,_,_ = pickle.load(open(chars_vocab_file,"wb"))
+	token2charsid,_,_ = pickle.load(open(chars_vocab_file,"rb"))
 	return token2charsid
 
 
